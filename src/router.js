e12dac6163211d0c4dfe5cac567409868c43f973
imsamtar <imsamtar@gmail.com>
2019-05-04 14:48:30 +0500

    Enable history mode in Vue Router (router.js)...

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-//   mode: 'history',
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
@@ -21,5 +21,10 @@ export default new Router({
     //   // which is lazy-loaded when the route is visited.
     //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     // }
+    {
+      path: '*',
+      name: 'all',
+      redirect: '/'
+    }
   ]
 })
