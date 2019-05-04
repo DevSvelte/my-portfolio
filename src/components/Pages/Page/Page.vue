2b9ac4e34bfc0b53368a9d0df8ae7ed5e88fc9f1
imsamtar <imsamtar@gmail.com>
2019-05-04 11:01:55 +0500

    Page component ready...

--- src/components/Pages/Page/Page.vue
+++ src/components/Pages/Page/Page.vue
@@ -1,10 +1,50 @@
 <template>
-    <section class="page container">
-        page
+    <section class="page" :style="mainStyles">
+        <h1 class="mytitle" v-if="title">{{title}}</h1>
+        <div class="container">
+            <slot></slot>
+        </div>
+        <div class="next" v-if="next">
+            <i class="fas fa-chevron-down"></i>
+        </div>
     </section>
 </template>
+<script>
+export default {
+    props: ['title','next','background'],
+    computed: {
+        mainStyles(){return {
+            background: this.background
+        }}
+    }
+}
+</script>
 <style scoped>
 .page {
     min-height: 100vh;
+    padding-top: 52px;
+}
+.mytitle {
+    text-align: center;
+    mix-blend-mode: difference;
+    text-transform: uppercase;
+    font-weight: 500 !important;
+    font-size: 25px;
+    letter-spacing: 10px;
+}
+.container {
+    mix-blend-mode: difference;
+    min-height: calc(100vh - 52px - 36px - 36px - 20px);
+}
+.next {
+    display: flex;
+    background: var(--color-b-2);
+    margin: auto;
+    height: 36px;
+    max-width: 200px;
+    place-items: center;
+    place-content: center;
+    margin-bottom: 20px;
+    font-size: 25px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
     <div id="home" class="hero is-primary">
-        
+        <skills />
+        <projects />
+        <about />
     </div>
 </template>
+<script>
+import Skills from '@/components/Pages/Skills.vue';
+import Projects from '@/components/Pages/Projects.vue';
+import About from '@/components/Pages/About.vue';
+
+export default {
+    components: { Skills, Projects, About }
+}
+</script>
+
