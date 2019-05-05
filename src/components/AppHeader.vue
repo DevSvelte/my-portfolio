7ad0d969aace71d9aff2ca90607ea9e051e661bf
imsamtar <imsamtar@gmail.com>
2019-05-05 16:26:10 +0500

    Added particalsJS to AppHeader component...

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,53 +2,95 @@
     <div @mousemove="mouseMove($event)">
     <app-navbar/>
     <header id="header">
-        <div class="container card">
-            <img class="profile-image" src="img/icons/android-icon-144x144.png" alt="">
-            <h1 class="subtitle has-text-white">SAMEER TARIQ</h1>
-            <h2 class="title has-text-white">I am a web developer!</h2>
+        <div class="container">
+            <img id="image" class="profile-image" src="img/icons/android-icon-144x144.png" alt="">
+            <h1 class="title" style="opacity: 1">SAMEER TARIQ</h1>
+            <h2>is a </h2>
+            <h1>WEB DEVELOPER &amp; DESIGNER</h1>
+            <div class="next">
+                <i class="fas fa-chevron-down"></i>
+            </div>
         </div>
     </header>
     </div>
 </template>
 <script>
 import AppNavbar from '@/components/AppNavbar.vue';
+
 export default {
     components: { AppNavbar },
+    data: () => ({
+        title : 'Hy there!'
+    }),
     methods: {
         mouseMove(e){
             let header = document.getElementById('header');
             header.style.setProperty('--x', `${100*e.screenX/window.innerWidth}%`);
-            header.style.setProperty('--y', `calc(${100*(e.screenY + window.scrollY)/window.innerHeight}% - 100px)`);
         }
+    },
+    created(){
+        // eslint-disable-next-line
+        particlesJS.load('header', '/particles.json');
     }
 }
 </script>
 
 <style scoped>
 #header {
-    --x: 50%;
-    --y: 50%;
-    background: radial-gradient(at var(--x) var(--y),#bb1bbb,#2a2aa3);
+    --x: 50%; --y: 50%;
+    background: linear-gradient(to bottom,var(--color-a),rgb(145, 19, 82)) ,black;
     min-height: 100vh;
     display: flex;
-    place-items: center;
-    padding-top: 52px;
-}
-#header>.container {
-    min-height: calc(100vh - 52px);
-    width: 100%;
-    background: transparent;
-    /* background: green; */
-    color: white;
-    display: grid;
-    grid-template-rows: repeat(3, max-content);
-    text-align: center;
     place-content: center;
 }
-#header>.container>img {
+#header>.container {
+    background: transparent;
+    border: 0px solid;
+    outline: 0px solid;
+    box-shadow: 0px;
+    display: grid;
+    position: absolute;
+    animation: 2s fadein;
+    top: 15vh;
+    max-width:350px;
+    /* filter: blur(5px); */
+    filter: drop-shadow(5px 5px 50px black);
+}
+#header>.container>* {
+    color: #fff;
+    font-size: 1.5rem;
+    text-align: center;
     margin: auto;
-    height: 122px;
+}
+#header>.container>img {
     border-radius: 50%;
+    /* border: 1px solid #0b0e19; */
     margin-bottom: 10px;
+    width: 200px;
+    /* mix-blend-mode: luminosity; */
+    /* filter: blur(10px); */
+    filter: hue-rotate(var(--hue, 30deg));
+}
+#header>.container>h1 {
+    opacity: 0.9;
+    font-weight: bold;
+}
+#header>.container>h2 {
+    opacity: 0.4;
+}
+#header>.container>.next {
+    display: grid;
+    background: var(--color-b-2);
+    height: 36px;
+    width: 100px;
+    place-content: center;
+    margin: 10px auto 20px auto;
+    font-size: 25px;
+    color: #ffffff77;
+    position: relative;
+    top: 80%;
+}
+@keyframes fadein {
+    from { opacity:0 }
 }
 </style>
