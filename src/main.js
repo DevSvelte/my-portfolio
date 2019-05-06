f2a5e9511ca3220333c467383dd504eeb147a93d
imsamtar <imsamtar@gmail.com>
2019-05-06 06:11:30 +0500

    Removed Service Worker...

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import store from './store'
-import './registerServiceWorker'
+// import './registerServiceWorker'
 import './assets/main.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  // router,
   store,
   render: h => h(App)
 }).$mount('#app')
