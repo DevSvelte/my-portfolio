1b2b1411bac8984f5bc4464e30c485d3b0d37642
imsamtar <imsamtar@gmail.com>
2019-05-07 21:22:41 +0500

    Made changes to projects in Projects component...

--- src/components/Pages/Projects.vue
+++ src/components/Pages/Projects.vue
@@ -29,9 +29,9 @@ export default {
             { img: '/img/projects/my-portfolio.png', github: '//github.com/imsamtar/my-portfolio', title: 'My Portfolio Website (This Site)', link: '//google.com'},
             { img: '/img/projects/my-todos.png', github: '//github.com/imsamtar/my-portfolio', title: 'My Todos App' , link: '//mytodos.ml'},
             { img: '/img/projects/hosterxml.png', github: '//github.com/imsamtar/hosterx.ml', title: 'HosterXML Web Hoster'  , link: '//google.com'},
-            { img: '//', github: '//', title: '', link: '//google.com'},
-            { img: '//', github: '//', title: '', link: '//google.com'},
-            { img: '//', github: '//', title: '', link: '//google.com'}
+            { img: '//', github: '//github.com/imsamtar', title: '#', link: '#'},
+            { img: '//', github: '//github.com/imsamtar', title: '#', link: '#'},
+            { img: '//', github: '//github.com/imsamtar', title: '#', link: '#'}
         ]
     })
 }
@@ -43,32 +43,37 @@ export default {
     grid-gap: 12px;
 }
 #projects > * {
-    padding: 15px 10px 10px 15px;
-    background: var(--color-b-2);
+    padding: 5px;
+    background: var(--color-b);
     text-align: center;
     border-radius: 5px;
 }
-.mysubtitle {
-    font-size: 20px;
-}
-#projects > * > a {
-    margin: 10px 0;
-    border-radius: 4px;
-    opacity: 0.6;
-}
+
 .snapshot {
     height: 180px;
     display: grid;
+    filter: blur(2px);
 }
+.snapshot:hover { filter: blur(0); }
 .snapshot > .github {
     opacity: 0;
-    background: black;
+    background: var(--color-b);
     display: grid;
     align-content: center;
     justify-content: center;
 }
 .snapshot > .github:hover {
     opacity: 0.95;
+}
+
+.mysubtitle {
+    margin: 7px;
+    font-size: 20px;
+}
+#projects > * > a {
+    margin: 10px 0;
+    border-radius: 4px;
+    opacity: 0.6;
 }
 @media screen and (max-width: 600px){
     #projects {
