d48bd0c4972394ea2a4cab138062e3118be981e0
Sameer Tariq <imsamtar@gmail.com>
2019-09-05 19:50:05 +0500

    Changed text in about.vue file

--- src/components/Pages/About.vue
+++ src/components/Pages/About.vue
@@ -6,13 +6,13 @@
             </div>
             <div class="column">
               <p class="has-text-justified">
-                  Hi! Thanks for being here! My name is Sameer Tariq. I am a creative web develepor holding minimum 3 year experience. Excellent work is always my aim. I am here to provide services in a more reliable and effective mode. 100% quality work is guaranteed. Cordially waiting for a chance to surprise you, Thank You!
+                  Hi! Thanks for being here! My name is Sameer Tariq. I am a creative web develepor holding minimum 3 year experience. Excellent work is always my aim. I am here to provide services in a more reliable and effective mode. 100% quality work is guaranteed. Cordially waiting for a chance to surprise you
               </p>
               <p class="has-text-justified">
                   You can hire me to create a website that is modern / attractive. So why waste any time just send me an email <a href="mail://imsamtar@gmail.com">imsamtar@gmail.com></a> and i will respond you.
               </p>
               <p>
-                  Thank You
+                  Thank You!
               </p>
             </div>
         </div>
