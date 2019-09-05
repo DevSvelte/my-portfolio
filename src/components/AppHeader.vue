7ff3bb91ab2c18d6ee156df0ab6983cb0439d4e7
Sameer Tariq <imsamtar@gmail.com>
2019-09-05 18:26:13 +0500

    Made little bug fix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,7 @@
     <app-navbar/>
     <header id="header">
         <div class="container">
-            <img id="image" class="profile-image" src="img/icons/android-icon-144x144.png" alt="">
+            <img id="image" class="profile-image" style="opacity: 0;" src="img/icons/android-icon-144x144.png" alt="">
             <h1 class="title" style="opacity: 1">SAMEER TARIQ</h1>
             <h2>is a </h2>
             <h1>WEB DEVELOPER &amp; DESIGNER</h1>
