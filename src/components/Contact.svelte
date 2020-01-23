886b81370dc932b88ddf5e48d5770dc34f950151
SAMEER TARIQ <imsamtar@gmail.com>
2020-01-23 14:29:42 +0500

    Craeted Lambda Function to send email

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -7,7 +7,20 @@
 
     onMount(() => {
         observer.observe(section);
-    })
+    });
+    let loading = false;
+    let defaultEmail = {name: '', email: '', subject: '', message: ''};
+    let email = {...defaultEmail};
+    async function sendEmail(){
+        loading = true;
+        let res = await (await fetch('/api/email', {
+            method: 'POST',
+            body: JSON.stringify(email)
+        })).json();
+        if(res.sent) email = {...defaultEmail};
+        else alert('Failed to send email');
+        loading = false;
+    }
 </script>
 
 <style>
@@ -56,6 +69,9 @@ h1 {
     user-select: none;
     font-weight: bold;
 }
+.disabled {
+    opacity: 0.7;
+}
 @media (min-width: 1025px){
     .container {
         width: 85%;
@@ -66,13 +82,13 @@ h1 {
 <section id="contact" bind:this={section}>
     <div class="container">
         <h1>Contact</h1>
-        <div class="form">
-            <input type="text" placeholder="Name">
-            <input type="text" placeholder="Email">
-            <input type="text" placeholder="Subject">
-            <textarea name="message" id="message" cols="40" rows="10" placeholder="Message"></textarea>
-            <button>Send Message</button>
-        </div>
+        <form class="form" on:submit|preventDefault={sendEmail}>
+            <input type="text" placeholder="Name" bind:value={email.name} required>
+            <input type="email" placeholder="Email" bind:value={email.email} required>
+            <input type="text" placeholder="Subject" bind:value={email.subject} required>
+            <textarea name="message" id="message" cols="40" rows="10" placeholder="Message" bind:value={email.message} required></textarea>
+            <button type="submit" class:disabled={!(email.name && email.email && email.subject && email.message) || loading}>Send Message</button>
+        </form>
         <Button target="hero" text="Back to top" color="#222222" />
     </div>
 </section>
