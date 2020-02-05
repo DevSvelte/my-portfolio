c6bbdf29439453b4ef1df00f7de89810a38656c4
SAMEER TARIQ <imsamtar@gmail.com>
2020-02-05 20:11:53 +0500

    Update serverless function

--- api/email.js
+++ api/email.js
@@ -1,45 +1,51 @@
 import nodemailer from 'nodemailer';
 
 export default (req, res) => {
-    try {
-        console.clear();
-    console.log(req.body);
-    let body = JSON.parse(req.body);
-    console.log(body);
-    if(req.method==='POST' && body.email && body.message){
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.email,
-                pass: process.env.password
-            }
-        });
-
-        let mailOptions = {
-            from: process.env.email,
-            to: process.env.to,
-            subject: `${body.subject || body.name || 'No Subject'} | from Portfolio`,
-            text: `${body.message}
+    if(req.method==='POST'){
+        try {
+            let body = JSON.parse(req.body);
+            if(body.email && body.message){
+                let transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: process.env.email,
+                        pass: process.env.password
+                    }
+                });
+                let mailOptions = {
+                    from: process.env.email,
+                    to: process.env.to,
+                    subject: `${body.subject || body.name || 'No Subject'} | from Portfolio`,
+                    text: `${body.message}
 Sender Name: ${body.name || 'Not Given'}
 Sender: ${body.email}
 `
-        };
+                };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                res.json({ sent: false, error: error.response });
-            } else {
-                res.json({ sent: true, response: info.response });
-            }
-        });
-    }
-    else {
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        res.json({ sent: false, message: error.response });
+                    } else {
+                        res.json({ sent: true, message: info.response });
+                    }
+                });
+        }
+        else {
+            res.json({
+                sent: false,
+                message: 'email or message not given in request body'
+            });
+        }
+        }catch(err){
+            res.json({
+                sent: false,
+                message: err.message
+            });
+        }
+    } else {
         res.json({
             sent: false,
-            message: 'Invalid Request'
+            message: 'Only method(POST) allowed'
         });
-    }
-    }catch(err){
-        console.log(err.message);
     }
 }
